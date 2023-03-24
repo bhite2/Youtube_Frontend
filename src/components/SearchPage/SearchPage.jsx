7e69dda578c99fd9b74518c657aaeedaff985d5f
Bryan Hite <bhite2@mail.usf.edu>
2023-03-24 15:18:12 -0400

    Search results return back thumbnail,title, and description.

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,25 +1,41 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const SearchPage = () => {
+const SearchPage = (props) => {
 
-    const [video, setVideo] = useState([]);
+    const [videos, setVideos] = useState([]);
 
     useEffect(() => {
         fetchVideos();
       }, [])
 
     async function fetchVideos() {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search?q=gravedigger&key=AIzaSyD5zvbsn67fOSLOXa_1AqsN7ijcWb-gOqk&part=snippet&maxresult=5q=gravedigger');
-        console.log(response.data)
-        setVideo(response.data);
+        const response = await axios.get('https://www.googleapis.com/youtube/v3/search?q=airjordan1&key=AIzaSyD5zvbsn67fOSLOXa_1AqsN7ijcWb-gOqk&part=snippet&maxresult=5q=gravedigger');
+        console.log(response.data.items)
+        setVideos(response.data.items);
       }
 
 
     return ( 
 
       <div>
-        Hello World
+         {videos.map((video) => {
+          return (
+          <div>  
+            <div>
+              <img src={video.snippet.thumbnails.medium.url}/>
+              
+            </div>
+            <div>
+              {video.snippet.title}
+            </div>
+            <div>
+              {video.snippet.description}
+            </div>
+          </div>
+          )
+         })}
+ 
       </div>
  
      );
